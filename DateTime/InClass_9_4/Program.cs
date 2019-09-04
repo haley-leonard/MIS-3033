515cfb68462a0b819511c3a3566157b49526293f
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-09-04 11:07:05 -0500

    in class stuff

---
+++ DateTime/InClass_9_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_9_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            DateTime future=today.AddDays(27);
+            Console.WriteLine(today.ToLongDateString());
+            Console.WriteLine(future);
+
+            var difference = future - today;
+
+            Console.WriteLine(difference.Days);
+
+            Console.ReadKey();
+        }
+    }
+}
