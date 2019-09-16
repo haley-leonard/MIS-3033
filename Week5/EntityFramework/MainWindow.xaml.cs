be7043e298929a424f799295fa753915a8265f18
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-09-16 11:28:35 -0500

    editing

--- Week5/EntityFramework/MainWindow.xaml.cs
+++ Week5/EntityFramework/MainWindow.xaml.cs
@@ -25,15 +25,37 @@ namespace EntityFramework
         public MainWindow()
         {
             InitializeComponent();
+            txtStudentID.Text = string.Empty;
         }
 
         private void btn_data_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var student in db.Students)
+            if (txtStudentID.Text==string.Empty)
             {
-                string temp = $"{student.LastName},{student.FirstName} ({student.StudentID}) likes {student.FavoriteColor}";
-                lst_data.Items.Add(temp);
+                foreach (var student in db.Students)
+                            {
+                                string temp = $"{student.LastName},{student.FirstName} ({student.StudentID}) likes {student.FavoriteColor}";
+                                lst_data.Items.Add(temp);
+                            }
             }
+            else
+            {
+                int id; //= Convert.ToInt32(txtStudentID.Text);
+                if(Int32.TryParse(txtStudentID.Text,out id))
+                {
+                    //db.Students.Where(student => student.StudentID == id);
+
+                    foreach (var student in db.Students)
+                    {
+                        if (student.StudentID == id)
+                        {
+                            string temp = $"{student.LastName},{student.FirstName} ({student.StudentID}) likes {student.FavoriteColor}";
+                            lst_data.Items.Add(temp);
+                        }
+                    }
+                }
+        }
+            
 
         }
     }
