71ad6810801398ae0b05d2eb5a65dfd7e843e8ae
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-10-25 11:18:31 -0500

    studying away

---
+++ Exam Two Study Guide/PokeAPI/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeAPI
+{
+    class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string ability { get; set; }
+    }
+    class Abilities
+    {
+        public List<Abilities> abilities { get; set; }
+    }
+}
